c68ec9bf2cfd859eeaa5d3862acc71e92fbea2f7
Carlos Chavez <cenric09@gmail.com>
2022-03-18 23:26:02 -0400

    added basic read and write functions for firestore

--- CloudHop/ViewController.swift
+++ CloudHop/ViewController.swift
@@ -15,9 +15,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+
+//        postDoc(collection: "users", data: ["name": "Carlos"])
+//        getDoc(collection: "users", document: "Carlos")
     }
 
+    func postDoc(collection: String, data: NSDictionary) {
+        // TODO: replace docID with email once auth is set up
+        
+//        let collectionRef = db.collection(collection)
+//        let docId = collectionRef.document().documentID
+        let name = data["name"] as! String
+        let documentRef = db.document("\(collection)/\(name)")
+        documentRef.setData(["text": name])
+        
+    }
+    
+    func getDoc(collection: String, document: String) {
+        let docRef = db.document("\(collection)/\(document)")
+        docRef.getDocument { snapshot, error in
+            guard let data = snapshot?.data(), error == nil else { return }
+            print(data)
+        }
+    }
 
 }
 
