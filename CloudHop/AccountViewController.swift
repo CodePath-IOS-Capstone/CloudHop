63b52604dc1e7832dc23915e36678ebded26f938
Eva Sennrich <evasennrich@gmail.com>
2022-03-25 18:45:30 -0400

    committing signIn/signOut connections

---
+++ CloudHop/AccountViewController.swift
@@ -0,0 +1,95 @@
+//
+//  AccountViewController.swift
+//  CloudHop
+//
+//  Created by Eva Sennrich on 3/24/22.
+//
+
+import UIKit
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class AccountViewController: UIViewController {
+
+    let db = Firestore.firestore()
+    var userEmail = ""
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func signInUser(_ sender: Any) {
+        signIn(email: emailField.text!, password: passwordField.text!)
+        
+    }
+    
+    @IBAction func signUpUser(_ sender: Any) {
+        // takes the user to the register page
+                self.performSegue(withIdentifier: "SignUpSegue", sender: nil)
+
+    }
+    
+    
+    /*
+    Function to sign in to an existing user account with email and password
+    */
+    func signIn(email: String, password: String) {
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { [weak self] result, error in
+            guard let strongSelf = self else {
+                return
+            }
+            guard error == nil else {
+//                strongSelf.showCreateAccount() // could not sign in, no account exists.
+                print("Error, no user exists.")
+                return
+            }
+            print("Signed in")
+            self!.performSegue(withIdentifier: "homeScreenSegue", sender: nil)
+        })
+        
+        
+//        if FirebaseAuth.Auth.auth().currentUser != nil {
+//            userEmail = FirebaseAuth.Auth.auth().currentUser!.email!
+//            self.performSegue(withIdentifier: "homeScreenSegue", sender: nil)
+//        } else {
+//            print("Not signed in.")
+//        }
+    }
+        
+        
+        
+        
+//        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+//          guard let strongSelf = self else { return
+//            if user != nil {
+//                self.performSegue(withIdentifier: "homeScreenSegue", sender: nil)
+//            } else {
+//                print("error")
+//            }
+//        }
+//        }
+        
+        
+    }
+    
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ CloudHop/SignUpViewController.swift
@@ -0,0 +1,87 @@
+//
+//  SignUpViewController.swift
+//  CloudHop
+//
+//  Created by Eva Sennrich on 3/24/22.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+
+class SignUpViewController: UIViewController {
+
+    let db = Firestore.firestore()
+   
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var countryField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        self.view.backgroundColor = UIColor.red
+    }
+    
+    
+    @IBAction func SignUp(_ sender: Any) {
+        
+        signUpUser(email: emailField.text!, password: passwordField.text!, name: nameField.text!, country: countryField.text!)
+    
+    }
+    
+    @IBAction func SignIn(_ sender: Any) {
+//        self.performSegue(withIdentifier: "signInAlreadyHaveAccount", sender: nil)
+    }
+    
+    /*
+    Function to create an account and sign up with email and password. Creates the user document once the user is signed up
+    */
+    func signUpUser(email: String, password: String, name: String, country: String) {
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { [weak self] result, error in
+            guard let strongSelf = self else {
+                return
+            }
+            guard error == nil else {
+                print("There was an error.")
+                return
+            }
+//             save user email, name and country into doc under email
+            self?.createNewUserDocument(data: ["name" : name, "email" : email, "country" : country])
+            self?.performSegue(withIdentifier: "homeSegue", sender: nil)
+            print("Signed in")
+            
+        })
+    }
+    
+    /*
+    Create a new user in the users collection. Call on user account creation
+    */
+    func createNewUserDocument(data: NSDictionary) {
+        
+        let name = data["name"] as! String
+        let email = data["email"] as! String
+        let country = data["country"] as! String
+//        let createdAt = data["createdAt"] as! Date
+        
+        let documentRef = db.document("users/\(email)")
+
+        // TODO: add the createdAt date
+        documentRef.setData(["name": name, "email": email, "country": country])
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
