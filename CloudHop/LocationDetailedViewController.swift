8322045cb67cd900e4ada6bef7cf9f4958a6a89b
Eva Sennrich <evasennrich@gmail.com>
2022-04-12 17:04:39 -0400

    tabBar changes completed for homeView,exploreView,profileView.

--- CloudHop/LocationDetailedViewController.swift
+++ CloudHop/LocationDetailedViewController.swift
@@ -13,15 +13,31 @@ class LocationDetailedViewController: UIViewController {
     @IBOutlet weak var locationTitle: UILabel!
     @IBOutlet weak var countryTitle: UILabel!
     @IBOutlet weak var descriptionBox: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var checkedB = Bool()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        likeButton.layer.shadowColor = UIColor.black.cgColor
+        likeButton.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        likeButton.layer.shadowRadius = 3
+        likeButton.layer.shadowOpacity = 0.5
+        
     }
     
     @IBAction func likeOnClick(_ sender: Any) {
-        
+       
+//        if !checkedB {
+//            likeButton.setImage(UIImage(named: "heart.png"), for: .normal)
+//            UserUtil.addLikedLocation(location: locationTitle.text!)
+//            checkedB = true
+//        } else {
+//            likeButton.setImage(UIImage(named: "unfavorite.png"), for: .normal)
+//            UserUtil.deleteLike(email: UserUtil.userEmail, field: locationTitle.text!)
+//            checkedB = false
+//        }
         
     }
     
